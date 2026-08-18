eb9dd41246dfab5203b8f81581ef53596a8a59fc
Eugene Bulgakov <zoom81@yandex.ru>
2026-08-18 23:58:48 +0400

    Refactor layout and navigation structure; remove unused components

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,7 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useColorScheme } from "react-native";
-
-import { AnimatedSplashOverlay } from "@/components/animated-icon";
-import AppTabs from "@/components/app-tabs";
+import { Tabs } from "expo-router";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -11,8 +9,10 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
+      <Tabs>
+        <Tabs.Screen name="index" options={{ title: "Home page" }} />
+        <Tabs.Screen name="explore" options={{ title: "Explore App" }} />
+      </Tabs>
     </ThemeProvider>
   );
 }
